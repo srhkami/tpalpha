65f5a1e9299beec6b96481a9ce3a81955a44214a
srhkami <160311248+srhkami@users.noreply.github.com>
2024-06-04 16:07:50 +0800

    1.14.1

--- script/base/nav.js
+++ script/base/nav.js
@@ -8,9 +8,12 @@ const headerHtml = `
       aria-controls="offcanvasNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand d-flex fw-bolder ms-3" href="../index.html">
+    <a class="navbar-brand d-none d-lg-flex fw-bolder ms-3" href="../index.html">
       <img src="../icons/icon_trafficpigeon_2.png" alt="Logo" width="30" height="30" class="my-auto me-2">
       <span id="pageTitle">交通鴿手</span>
+    </a>
+    <a class="navbar-brand fw-bolder ms-3" href="#">
+      處罰條例
     </a>
     <!-- 主題切換按鈕 -->
     <div class="dropdown ms-auto">
@@ -115,174 +118,184 @@ function loadBookmark(){
 // 側邊欄
   // 預設關閉的在a加上.collapsed，預設打開的在div加上.show
 const asideHtml = `
-  <div id="menuBookmark" class="p-1 mb-2 border-start border-3 border-primary">
-    <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(0)[0]}" data-menu-no="0" data-bs-toggle="collapse" href="#collapseExample" role="button">
-      <img class="i-15 me-1" src="../icons/icon_bookmark.png" alt="">
-      <span>書籤</span>
-      <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
-        <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
-      </svg>
-    </a>
-    <div class="collapse ${loadSidebar(0)[1]}" id="collapseExample">
-      <ul class="m-0" style="list-style-type:none">
-        ${loadBookmark()}
-      </ul>
-    </div>
+  <div class="offcanvas-header ps-2 mb-2">
+  <a class="navbar-brand d-flex fw-bolder" href="../index.html">
+    <img src="../icons/icon_trafficpigeon_2.png" alt="Logo" width="30" height="30" class="my-auto me-2">
+    <h4 class="offcanvas-title ms-1" id="offcanvasNavbarLabel">交通鴿手</h4>
+  </a>
+  <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
   </div>
-  <div id="menu1" class="p-1 my-2 border-start border-3 border-primary">
-    <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(1)[0]}" data-menu-no="1"  data-bs-toggle="collapse" href="#collapseExample1" role="button">
-      <img class="i-15 me-1" src="../icons/icon_highWay.png" alt="">
-      <span>交通法規</span>
-      <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
-        <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
-      </svg>
-    </a>
-    <div class="collapse ${loadSidebar(1)[1]}" id="collapseExample1">
-      <ul class="m-0" style="list-style-type:none">
-        <li>
-          <a class="list-group-item py-1" href="..${pages.PH.href}">
-            <img class="i-15 me-1" src="..${pages.PH.icon}" alt="">
-            ${pages.PH.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.SR.href}">
-            <img class="i-15 me-1" src="..${pages.SR.icon}" alt="">
-            ${pages.SR.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.DR.href}">
-            <img class="i-15 me-1" src="..${pages.DR.icon}" alt="">
-            ${pages.DR.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.ML.href}">
-            <img class="i-15 me-1" src="..${pages.ML.icon}" alt="">
-            ${pages.ML.title}
-          </a>
-        </li>
-        <li>
-          <hr class="m-1">
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.TA.href}">
-            <img class="i-15 me-1" src="..${pages.TA.icon}" alt="">
-            ${pages.TA.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.VS.href}">
-            <img class="i-15 me-1" src="..${pages.VS.icon}" alt="">
-            ${pages.VS.title}
-          </a>
-        </li>
-      </ul>
+  <!-- 主選單清單 -->
+  <div id="pageMenu" class="rounded-3 px-2 h-100 overflow-auto">  
+    <div id="menuBookmark" class="p-1 mb-2 border-start border-3 border-primary">
+      <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(0)[0]}" data-menu-no="0" data-bs-toggle="collapse" href="#collapseExample" role="button">
+        <img class="i-15 me-1" src="../icons/icon_bookmark.png" alt="">
+        <span>書籤</span>
+        <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
+          <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
+        </svg>
+      </a>
+      <div class="collapse ${loadSidebar(0)[1]}" id="collapseExample">
+        <ul class="m-0" style="list-style-type:none">
+          ${loadBookmark()}
+        </ul>
+      </div>
     </div>
-  </div>
-  <div id="menu2" class="p-1 my-2 border-start border-3 border-primary">
-    <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(2)[0]}" data-menu-no="2" data-bs-toggle="collapse" href="#collapseExample2"
-    role="button">
-      <img class="i-15 me-1" src="../icons/icon_quickSearch.png" alt="">
-      <span>違規取締</span>
-      <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
-        <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
-      </svg>
-    </a>
-    <div class="collapse ${loadSidebar(2)[1]}" id="collapseExample2">
-      <ul class="m-0" style="list-style-type:none">
-        <li>
-          <a class="list-group-item py-1" href="..${pages.DL.href}">
-            <img class="i-15 me-1" src="..${pages.DL.icon}" alt="">
-            ${pages.DL.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.CL.href}">
-            <img class="i-15 me-1" src="..${pages.CL.icon}" alt="">
-            ${pages.CL.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.overload.href}">
-            <img class="i-15 me-1" src="..${pages.overload.icon}" alt="">
-            ${pages.overload.title}
-          </a>
-        </li>
-        <li>
-          <hr class="m-1">
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.keypoint.href}">
-            <img class="i-15 me-1" src="..${pages.keypoint.icon}" alt="">
-            ${pages.keypoint.title}
-          </a>
-        </li>
-      </ul>
+    <div id="menu1" class="p-1 my-2 border-start border-3 border-primary">
+      <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(1)[0]}" data-menu-no="1"  data-bs-toggle="collapse" href="#collapseExample1" role="button">
+        <img class="i-15 me-1" src="../icons/icon_highWay.png" alt="">
+        <span>交通法規</span>
+        <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
+          <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
+        </svg>
+      </a>
+      <div class="collapse ${loadSidebar(1)[1]}" id="collapseExample1">
+        <ul class="m-0" style="list-style-type:none">
+          <li>
+            <a class="list-group-item py-1" href="..${pages.PH.href}">
+              <img class="i-15 me-1" src="..${pages.PH.icon}" alt="">
+              ${pages.PH.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.SR.href}">
+              <img class="i-15 me-1" src="..${pages.SR.icon}" alt="">
+              ${pages.SR.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.DR.href}">
+              <img class="i-15 me-1" src="..${pages.DR.icon}" alt="">
+              ${pages.DR.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.ML.href}">
+              <img class="i-15 me-1" src="..${pages.ML.icon}" alt="">
+              ${pages.ML.title}
+            </a>
+          </li>
+          <li>
+            <hr class="m-1">
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.TA.href}">
+              <img class="i-15 me-1" src="..${pages.TA.icon}" alt="">
+              ${pages.TA.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.VS.href}">
+              <img class="i-15 me-1" src="..${pages.VS.icon}" alt="">
+              ${pages.VS.title}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  <div id="menu3" class="p-1 my-2 border-start border-3 border-primary">
-    <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(3)[0]}" data-menu-no="3" data-bs-toggle="collapse" href="#collapseExample3" role="button">
-      <img class="i-15 me-1" src="../icons/icon_law_2.png" alt="">
-      <span>警察法規</span>
-      <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
-        <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
-      </svg>
-    </a>
-    <div class="collapse ${loadSidebar(3)[1]}" id="collapseExample3">
-      <ul class="m-0" style="list-style-type:none">
-        <li>
-          <a class="list-group-item py-1" href="..${pages.PA.href}">
-            <img class="i-15 me-1" src="..${pages.PA.icon}" alt="">
-            ${pages.PA.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.PW.href}">
-            <img class="i-15 me-1" src="..${pages.PW.icon}" alt="">
-            ${pages.PW.title}
-          </a>
-        </li>
-        <li>
-          <hr class="m-1">
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.CC.href}">
-            <img class="i-15 me-1" src="..${pages.CC.icon}" alt="">
-            ${pages.CC.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.CP.href}">
-            <img class="i-15 me-1" src="..${pages.CP.icon}" alt="">
-            ${pages.CP.title}
-          </a>
-        </li>
-        <li>
-          <a class="list-group-item py-1" href="..${pages.SO.href}">
-            <img class="i-15 me-1" src="..${pages.SO.icon}" alt="">
-            ${pages.SO.title}
-          </a>
-        </li>
-      </ul>
+    <div id="menu2" class="p-1 my-2 border-start border-3 border-primary">
+      <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(2)[0]}" data-menu-no="2" data-bs-toggle="collapse" href="#collapseExample2"
+      role="button">
+        <img class="i-15 me-1" src="../icons/icon_quickSearch.png" alt="">
+        <span>違規取締</span>
+        <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
+          <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
+        </svg>
+      </a>
+      <div class="collapse ${loadSidebar(2)[1]}" id="collapseExample2">
+        <ul class="m-0" style="list-style-type:none">
+          <li>
+            <a class="list-group-item py-1" href="..${pages.DL.href}">
+              <img class="i-15 me-1" src="..${pages.DL.icon}" alt="">
+              ${pages.DL.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.CL.href}">
+              <img class="i-15 me-1" src="..${pages.CL.icon}" alt="">
+              ${pages.CL.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.overload.href}">
+              <img class="i-15 me-1" src="..${pages.overload.icon}" alt="">
+              ${pages.overload.title}
+            </a>
+          </li>
+          <li>
+            <hr class="m-1">
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.keypoint.href}">
+              <img class="i-15 me-1" src="..${pages.keypoint.icon}" alt="">
+              ${pages.keypoint.title}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  <div class="p-1 my-2 border-start border-3 border-primary">
-    <a class="menu-item dropdown-item d-flex mb-1" href="..${pages.feedback.href}">
-      <img class="i-15 me-1" src="..${pages.feedback.icon}" alt="">
-      <span>${pages.feedback.title}</span>
-    </a>
-  </div>
-  <div class="p-1 my-1 border-start border-3 border-primary">
-    <a class="dropdown-item d-flex mb-1" href="..${pages.about.href}">
-      <img class="i-15 me-1" src="..${pages.about.icon}" alt="">
-      <span>${pages.about.title}</span>
-    </a>
+    <div id="menu3" class="p-1 my-2 border-start border-3 border-primary">
+      <a class="menu-item dropdown-item d-flex mb-1 ${loadSidebar(3)[0]}" data-menu-no="3" data-bs-toggle="collapse" href="#collapseExample3" role="button">
+        <img class="i-15 me-1" src="../icons/icon_law_2.png" alt="">
+        <span>警察法規</span>
+        <svg class="anime-icon i-10 ms-auto my-auto" fill="#5A96FA">
+          <use xlink:href="../icons/bootstrap-icons.svg#chevron-right"></use>
+        </svg>
+      </a>
+      <div class="collapse ${loadSidebar(3)[1]}" id="collapseExample3">
+        <ul class="m-0" style="list-style-type:none">
+          <li>
+            <a class="list-group-item py-1" href="..${pages.PA.href}">
+              <img class="i-15 me-1" src="..${pages.PA.icon}" alt="">
+              ${pages.PA.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.PW.href}">
+              <img class="i-15 me-1" src="..${pages.PW.icon}" alt="">
+              ${pages.PW.title}
+            </a>
+          </li>
+          <li>
+            <hr class="m-1">
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.CC.href}">
+              <img class="i-15 me-1" src="..${pages.CC.icon}" alt="">
+              ${pages.CC.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.CP.href}">
+              <img class="i-15 me-1" src="..${pages.CP.icon}" alt="">
+              ${pages.CP.title}
+            </a>
+          </li>
+          <li>
+            <a class="list-group-item py-1" href="..${pages.SO.href}">
+              <img class="i-15 me-1" src="..${pages.SO.icon}" alt="">
+              ${pages.SO.title}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  <div id="arriveTerm" class="card bg-primary-subtle border-2 mt-3">
-    <h6 class="card-header text-center text-primary fw-bolder">到案日期</h6>
-    <div class="card-body p-2 text-center"></div>
+    <div class="p-1 my-2 border-start border-3 border-primary">
+      <a class="menu-item dropdown-item d-flex mb-1" href="..${pages.feedback.href}">
+        <img class="i-15 me-1" src="..${pages.feedback.icon}" alt="">
+        <span>${pages.feedback.title}</span>
+      </a>
+    </div>
+    <div class="p-1 my-1 border-start border-3 border-primary">
+      <a class="dropdown-item d-flex mb-1" href="..${pages.about.href}">
+        <img class="i-15 me-1" src="..${pages.about.icon}" alt="">
+        <span>${pages.about.title}</span>
+      </a>
+      </div>
+    <div id="arriveTerm" class="card bg-primary-subtle border-2 my-3">
+      <h6 class="card-header text-center text-primary fw-bolder">到案日期</h6>
+      <div class="card-body p-2 text-center"></div>
+    </div>
   </div>
 `;
 
@@ -336,11 +349,11 @@ function date_calculate(){
 // 主程式
 if ($('title').html() == '首頁 - 交通鴿手'){
   $('#pageHeader').html(headerHtml.replaceAll('..','.'));
-  $('#pageMenu').html(asideHtml.replaceAll('..','.'));
+  $('#sidebar').html(asideHtml.replaceAll('..','.'));
 }
 else{
   $('#pageHeader').html(headerHtml);
-  $('#pageMenu').html(asideHtml);
+  $('#sidebar').html(asideHtml);
 }
 $('#websiteLicense').html(licenseHtml);
 $(document).ready(()=>{
